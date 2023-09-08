5df21f653237a62b5635c5051df77d7cf41d4a90
노지용 <115959559+jiyongnoh@users.noreply.github.com>
2023-09-08 15:31:37 +0900

    design: 유저 정보 컴포넌트 디자인 수정

--- src/component/User.js
+++ src/component/User.js
@@ -2,12 +2,23 @@ import styled from "styled-components";
 
 function User({ user }) {
   return (
-    <>
+    <UserContainer>
       <span>
         학번:{user.user_number} 이름:{user.user_name}
       </span>
-    </>
+    </UserContainer>
   );
 }
 
 export default User;
+
+const UserContainer = styled.main`
+  padding: 1rem;
+
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: flex-start;
+
+  border: 1px solid black;
+`;
